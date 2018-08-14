57be76a09d5a815d76c88d67387fd1c506e5c059
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2018-08-14 15:34:34 +0200

    initial commit

---
+++ LibLedgerCore/preinstall.py
@@ -0,0 +1,26 @@
+import platform
+import urllib
+import os
+
+libCoreVersion = "1.1.0"
+baseURL = "https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
+filePath = ""
+
+userPlatform = platform.system()
+
+bucketFile = ""
+filePath = libCoreVersion + "/ios"
+bucketFile = "libledger-core.dylib"
+
+opener = urllib.URLopener()
+
+endpointURL = baseURL + "/" + filePath
+finalFile = "lib/" + bucketFile
+
+if not os.path.exists("lib"):
+    os.makedirs("lib")
+
+print('Downloading ' + endpointURL + "/" + bucketFile + ' ...')
+opener.retrieve(endpointURL + "/" + bucketFile, finalFile)
+
+os.chmod(finalFile, 0755)
