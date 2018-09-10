0c518928bb84b955b19e970d19b69be0c292223c
meriadec <meriadec.pillet@gmail.com>
2018-09-10 18:16:59 +0200

    Clean package.json, update index.js

--- LibLedgerCore/index.js
+++ LibLedgerCore/index.js
@@ -1,7 +1,5 @@
-/** @format */
+import { NativeModules } from "react-native";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const { RCTCoreLibLedgerCore } = NativeModules;
 
-AppRegistry.registerComponent(appName, () => App);
+export default RCTCoreLibLedgerCore;
