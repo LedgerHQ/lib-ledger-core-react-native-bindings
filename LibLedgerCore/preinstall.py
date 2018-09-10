61697281f277a7e49520387af1c4063668cff703
meriadec <meriadec.pillet@gmail.com>
2018-09-10 18:09:05 +0200

    Update preinstall script

--- LibLedgerCore/preinstall.py
+++ LibLedgerCore/preinstall.py
@@ -1,30 +1,19 @@
-import platform
 import urllib
 import os
 
-exit()
+LIB_CORE_VERSION = "1.1.0"
+BASE_URL = "https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 
-# TODO: make s3 repo public
+def download_file(directory, file_name, output_dir):
+    """Download file on bucket"""
+    endpoint_url = BASE_URL + "/" + directory
+    final_file = "lib/" + output_dir + "/" + file_name
+    if not os.path.exists("lib/" + output_dir):
+        os.makedirs("lib/" + output_dir)
+    print('Downloading ' + endpoint_url + "/" + file_name + ' ...')
+    opener = urllib.URLopener()
+    opener.retrieve(endpoint_url + "/" + file_name, final_file)
+    os.chmod(final_file, 0o755)
 
-libCoreVersion = "1.1.0"
-baseURL = "https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
-filePath = ""
-
-userPlatform = platform.system()
-
-bucketFile = ""
-filePath = libCoreVersion + "/ios"
-bucketFile = "libledger-core.dylib"
-
-opener = urllib.URLopener()
-
-endpointURL = baseURL + "/" + filePath
-finalFile = "lib/" + bucketFile
-
-if not os.path.exists("lib"):
-    os.makedirs("lib")
-
-print('Downloading ' + endpointURL + "/" + bucketFile + ' ...')
-opener.retrieve(endpointURL + "/" + bucketFile, finalFile)
-
-os.chmod(finalFile, 0755)
+download_file(LIB_CORE_VERSION + "/ios", "libledger-core.dylib", "ios/x86")
+download_file(LIB_CORE_VERSION + "/android", "libledger-core.so", "android/x86")
