8bd1fd0cceb799faea11178003dd47f13f46d56b
Dimitri Sabadie <dimitri.sabadie@ledger.fr>
2019-02-25 16:10:59 +0100

    Another pass of fixing stuff. (setPromise, this time)

--- android/src/main/java/com/ledger/reactnative/DatabaseBlobImpl.java
+++ android/src/main/java/com/ledger/reactnative/DatabaseBlobImpl.java
@@ -1,13 +1,19 @@
 package com.ledger.reactnative;
 
 import co.ledger.core.DatabaseBlob;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 
 public class DatabaseBlobImpl extends DatabaseBlob {
     private ReactApplicationContext reactContext;
+    private Promise promise;
 
     public DatabaseBlobImpl(ReactApplicationContext reactContext) {
         this.reactContext = reactContext;
+    }
+
+    public void setPromise(Promise _promise) {
+        this.promise = _promise;
     }
 
     public byte[] read(long offset, long length) {
--- android/src/main/java/com/ledger/reactnative/DatabaseConnectionImpl.java
+++ android/src/main/java/com/ledger/reactnative/DatabaseConnectionImpl.java
@@ -3,13 +3,19 @@ package com.ledger.reactnative;
 import co.ledger.core.DatabaseBlob;
 import co.ledger.core.DatabaseConnection;
 import co.ledger.core.DatabaseStatement;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 
 public class DatabaseConnectionImpl extends DatabaseConnection {
     private ReactApplicationContext reactContext;
+    private Promise promise;
 
     public DatabaseConnectionImpl(ReactApplicationContext reactContext) {
         this.reactContext = reactContext;
+    }
+
+    public void setPromise(Promise _promise) {
+        this.promise = _promise;
     }
 
     public DatabaseStatement prepareStatement(String query, boolean repeatable) {
