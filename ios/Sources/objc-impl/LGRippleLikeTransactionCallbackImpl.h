d70b255bbc4c8043fd73a1fa7a6372a13aff256d
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2019-04-18 22:29:18 +0200

    Add missing LGRippleLikeTransactionCallbackImpl

---
+++ ios/Sources/objc-impl/LGRippleLikeTransactionCallbackImpl.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+#import "../objc/LGRippleLikeTransactionCallback.h"
+
+@interface LGRippleLikeTransactionCallbackImpl : NSObject<LGRippleLikeTransactionCallback>
+
+@end
