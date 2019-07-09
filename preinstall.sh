9eb9e740aae116605a6a86974a5aaee5e11c2b67
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2019-07-09 16:26:21 +0200

    Bump libcore to stable release 3.0.0

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,7 @@
 #                              Preinstall script
 #
 
-LIB_CORE_VERSION="3.0.0-rc-fb6191"
+LIB_CORE_VERSION="3.0.0"
 BASE_URL="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 
 function main() {
