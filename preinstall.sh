5d288a5e9fb1613527f7fad7239d89b7d80ec628
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-11-05 16:51:49 +0100

    libcore 3.1.0-rc-1d3590

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,7 @@
 #                              Preinstall script
 #
 
-LIB_CORE_VERSION="3.1.0-rc-a7db8f"
+LIB_CORE_VERSION="3.1.0-rc-1d3590"
 BASE_URL="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 
 function main() {
