0fad5d7429671efb11f5868e9c3d92cd3d6f9515
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-02-18 15:01:52 +0100

    bump libcore version to same as on node bindings

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,7 @@
 #                              Preinstall script
 #
 
-LIB_CORE_VERSION="3.3.0-rc-aed1c9"
+LIB_CORE_VERSION="3.3.0-rc-c12b7d"
 BASE_URL="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 
 function main() {
