ee01e6b6f822e9e2148438701e00f3b0a0b68533
Henri Ly <henri@softlydev.com>
2020-03-10 18:18:17 +0100

    [Stellar] Add missing impl

---
+++ ios/Sources/objc-impl/LGStellarLikeAccountSignerListCallbackImpl.m
@@ -0,0 +1,7 @@
+#import "LGStellarLikeAccountSignerListCallbackImpl.h"
+
+@implementation LGStellarLikeAccountSignerListCallbackImpl
+- (void)onCallback:(nullable NSArray<LGStellarLikeAccountSigner *> *)result
+             error:(nullable LGError *)error
+{}
+@end
