caa704e5626f9619b49e4bc03e1d8c10353669b0
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-06-26 17:09:04 +0200

    3.4.1-rc-0a5acc

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,7 @@
 #                              Preinstall script
 #
 
-LIB_CORE_VERSION="3.4.1-rc-ac6350"
+LIB_CORE_VERSION="3.4.1-rc-0a5acc"
 BASE_URL="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 
 function main() {
