df1494fd69392ee287150f7be36ccc744eba2dbf
Alexis Le Provost <alexis.le-provost@ledger.fr>
2020-08-19 11:10:14 +0200

    bump lib-core version

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,7 @@
 #                              Preinstall script
 #
 
-LIB_CORE_VERSION="3.5.0-rc-8796ba"
+LIB_CORE_VERSION="3.5.0-rc-323f22"
 BASE_URL="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 
 function main() {
