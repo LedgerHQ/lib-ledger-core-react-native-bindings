256217708af6a898c8cfbba60ecdfa668cf5f18c
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-09-18 11:03:32 +0200

    rollback libcore version (of branch live-prerelease, not develop)

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,7 @@
 #                              Preinstall script
 #
 
-LIB_CORE_VERSION="3.5.0-rc-44d218"
+LIB_CORE_VERSION="3.5.0-rc-5587f2"
 BASE_URL="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 
 function main() {
