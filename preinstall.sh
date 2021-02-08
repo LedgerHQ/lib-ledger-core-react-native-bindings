31c20dbea44a28baa2dece193192bb8424f5b303
Gerry Agbobada <gerry.agbobada-ext@ledger.fr>
2021-02-08 12:19:11 +0100

    rc for 2.22: cosmos chainID to cosmoshub-4

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,7 @@
 #                              Preinstall script
 #
 
-LIB_CORE_VERSION="3.5.0-rc-8c0c4b"
+LIB_CORE_VERSION="3.5.0-rc-8823f3"
 BASE_URL="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 
 function main() {
