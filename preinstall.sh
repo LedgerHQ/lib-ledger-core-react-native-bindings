08fdb110ecd652852af141d2bad3da65aa140995
Gerry Agbobada <gerry.agbobada-ext@ledger.fr>
2021-02-18 18:40:56 +0100

    Bump Libcore
        - Add a 5s TTL on AlgorandAccount::getAccountInfo to lighten load on
          nodes
        - Fix the currency definition of ATOM that broke initial synchronization
          on Stargate
        - Forcefully wait for async functions during Cosmos account update calls
          to avoid Wallet Released issues coming from dangling promises

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,7 @@
 #                              Preinstall script
 #
 
-LIB_CORE_VERSION="3.5.0-rc-8823f3"
+LIB_CORE_VERSION="3.5.0-rc-1f2d11"
 BASE_URL="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 
 function main() {
