334bc2bc236e70b6c719e978a59536226f1094d8
hzheng-ledger <71653044+hzheng-ledger@users.noreply.github.com>
2021-03-16 09:44:15 +0100

    upgrade libcore version (#81)

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,7 @@
 #                              Preinstall script
 #
 
-LIB_CORE_VERSION="3.5.0-rc-e5f814"
+LIB_CORE_VERSION="3.5.0-rc-661687"
 BASE_URL="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 
 function main() {
